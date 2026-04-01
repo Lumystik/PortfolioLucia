7715a44cb661825122f8e43e7a6a021cbe459434
Lucía Medina <38432259+Lumystik@users.noreply.github.com>
2026-04-01 22:00:59 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,4 @@
 import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
-
-
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
@@ -11,6 +6,5 @@ export default defineConfig({
   base: "/PortfolioLucia/",
   plugins: [react()]
 })
-  
   
 
