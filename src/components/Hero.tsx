0f006f3fea728a2dda5125bbd7bf46a60e9d0316
Lucía Medina <38432259+Lumystik@users.noreply.github.com>
2026-04-02 11:04:23 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -100,7 +100,7 @@ export function Hero() {
         className="absolute inset-0 z-10 pointer-events-none"
         style={{ clipPath }}
       >
-        <img src="https://github.com/Lumystik/PortfolioLucia/blob/41551300955931cd9960ed03c816048092d92a9a/images/meedited.jpg className="w-full h-full object-cover" alt="Hero Background Sharp" />
+        <img src="https://github.com/Lumystik/PortfolioLucia/blob/41551300955931cd9960ed03c816048092d92a9a/images/meedited.jpg" className="w-full h-full object-cover" alt="Hero Background Sharp" />
         <WireframeSketches color="rgba(211, 211, 211, 0.65)" />
       </motion.div>
 
