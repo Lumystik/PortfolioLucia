c57c1386a7310f860ff710f0e01f0b1fc4691f51
Lucía Medina <38432259+Lumystik@users.noreply.github.com>
2026-04-02 05:28:03 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,19 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { ProjectDetail } from './pages/ProjectDetail';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="bg-white text-[#131313] min-h-screen selection:bg-black selection:text-white font-sans">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/project/:id" element={<ProjectDetail />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
