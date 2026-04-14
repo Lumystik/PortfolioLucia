20a2a324759b442af0991eb211f71140c7a5c96d
Lucía Medina <38432259+Lumystik@users.noreply.github.com>
2026-04-14 19:18:24 +0200

    Update ProjectDetail.tsx

--- src/pages/ProjectDetail.tsx
+++ src/pages/ProjectDetail.tsx
@@ -48,7 +48,7 @@ const projectsData: Record<string, any> = {
  description: "The design system was built to be clean, accessible, and highly functional, prioritizing data visualization and task management. It includes standardized colors, buttons, dropdown menus, and calendar components.",
   logoDescription: "We were drawn to the asterisk as a symbol of connection, and wanted to incorporate its radiating structure as a mark that points toward information, toward people, toward the institution itself.",
  logoImage: "https://github.com/Lumystik/PortfolioLucia/blob/ac040a0ff68481a4ef9745568ded5b5c9b484a5b/images/museonlogo.png?raw=true",       // optional
-  featuresImage: "https://github.com/Lumystik/PortfolioLucia/blob/dfc97077f6303fd7953feeb6d4e1a8efdce42b2e/images/button.png?raw=true" // optional
+  featuresImage: "https://github.com/Lumystik/PortfolioLucia/blob/dfc97077f6303fd7953feeb6d4e1a8efdce42b2e/images/button.png?raw=true", // optional
 
   shapes: [
     { color: "#000000", size: 120, x: "20%", y: "10%" },
