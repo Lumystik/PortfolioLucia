b12c72ea1e3d16434b81b2f90410ca70e479ed96
Lucía Medina <38432259+Lumystik@users.noreply.github.com>
2026-04-14 19:22:41 +0200

    Update ProjectDetail.tsx

--- src/pages/ProjectDetail.tsx
+++ src/pages/ProjectDetail.tsx
@@ -82,13 +82,12 @@ const projectsData: Record<string, any> = {
     },
     interfaceImage:"https://github.com/Lumystik/PortfolioLucia/blob/988251ade7a50360d870b2b630818e7291a7d458/images/museon_finalinterface%20(10).gif?raw=true",
     takeaways: "We solved the complexity of creating a smooth experience for small museum staff, enabling them to develop a better experience for visitors. By developing an all-in-one software, we streamlined the workflow and optimized current museum resources.",
-    interfaceImage: "https://github.com/Lumystik/PortfolioLucia/blob/988251ade7a50360d870b2b630818e7291a7d458/images/museon_finalinterface%20(10).gif?raw=true",
     nextProject: {
       title: "ART BEYOND DIMENSION",
       link: "/project/art-beyond-dimension",
       image: "https://github.com/Lumystik/PortfolioLucia/blob/c39ead2c3b7c47add54a5363ffb7b42cb35b1adf/images/wall.jpg?raw=true"
     }
-  },
+   },
   "art-beyond-dimension": {
     id: "art-beyond-dimension",
     title: "Art Beyond Dimension",
